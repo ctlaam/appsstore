9cf59f7a824b1b6a49712b04396ec4a612aa0376
hoainam-99 <hoainam12121999@gmail.com>
2023-09-06 09:41:11 +0700

    fix header

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const container = document.getElementById("container");
 const subHeader = document.getElementById("sub-header");
+const phoneHeader = document.getElementById("phone-header");
 
 container.addEventListener("scroll", (e) => {
     const { scrollTop, scrollHeight } = e.target;
     if(scrollTop < 100 && !subHeader.classList.contains("hidden")) {
         subHeader.classList.add("hidden");
+        phoneHeader.style.background = '#000';
     } else if(scrollTop >= 100 && subHeader.classList.contains("hidden")) {
+        phoneHeader.style.background = 'linear-gradient(#181818, #282828)';
         subHeader.classList.remove("hidden");
     }
 });
